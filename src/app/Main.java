cc671b405be23476f5a4f4c270870c7b16931589
Oleh Bilynskyi <obslinskij@gmail.com>
2025-04-09 16:31:38 +0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,7 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("A tool for converting miles to kilometers and kilometers to miles.");
+    }
+}
