5a386827196f6e558ca479c095da0acc1eaa1fc4
Oleh Bilynskyi <obslinskij@gmail.com>
2025-04-09 17:15:52 +0300

    Add functionality

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,5 +3,11 @@ package app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("A tool for converting miles to kilometers and kilometers to miles.");
+        double miles = 10;
+        double kilometers = convertMilesToKilometers(miles);
+        System.out.println(miles + " miles = " + kilometers + " km");
+    }
+    public static double convertMilesToKilometers(double miles) {
+        return miles * 1.60934;
     }
 }
